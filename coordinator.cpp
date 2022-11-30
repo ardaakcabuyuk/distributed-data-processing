41fb17d992046d52839280974be70a562b489707
abuyuk <abuyuk@blueoceansp.ai>
2022-11-30 19:42:32 +0100

    project final

--- coordinator.cpp
+++ coordinator.cpp
@@ -12,20 +12,26 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
+#include <vector>
+#include <map>
+#include <queue>
 
 using namespace std::literals;
 
 #define BACKLOG 10   // how many pending connections queue will hold
+#define MAXDATASIZE 1024 // max number of bytes we can get at once 
+#define TRUE 1
 
 void sigchld_handler(int s) {
    // waitpid() might overwrite errno, so we save and restore it:
    int saved_errno = errno;
-   while(waitpid(-1, NULL, WNOHANG) > 0 || s);
+   while(waitpid(-1, NULL, WNOHANG) > 0);
    errno = saved_errno;
+   int temp = s;
+   temp++;
 }
 
-void *get_in_addr(struct sockaddr *sa)
-{
+void *get_in_addr(struct sockaddr *sa) {
     if (sa->sa_family == AF_INET) {
         return &(((struct sockaddr_in*)sa)->sin_addr);
     }
@@ -45,99 +51,21 @@ int main(int argc, char* argv[]) {
       return 1;
    }
 
-   // TODO:
-   //    1. Allow workers to connect
-   //       socket(), bind(), listen(), accept(), see: https://beej.us/guide/bgnet/html/#system-calls-or-bust
-   //    2. Distribute the following work among workers
-   //       send() them some work
-   //    3. Collect all results
-   //       recv() the results
-   // Hint: Think about how you track which worker got what work
-
-   const char* PORT = argv[2];
-   int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
-   struct addrinfo hints, *servinfo, *p;
-   struct sockaddr_storage their_addr; // connector's address information
-   socklen_t sin_size;
-   struct sigaction sa;
-   int yes=1;
-   char s[INET6_ADDRSTRLEN];
-   int rv;
-
-   memset(&hints, 0, sizeof hints);
-   hints.ai_family = AF_UNSPEC;
-   hints.ai_socktype = SOCK_STREAM;
-   hints.ai_flags = AI_PASSIVE; // use my IP
-
-   if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
-      fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-      return 1;
-   }
-
-   // loop through all the results and bind to the first we can
-   for(p = servinfo; p != NULL; p = p->ai_next) {
-      if ((sockfd = socket(p->ai_family, p->ai_socktype,
-               p->ai_protocol)) == -1) {
-         perror("server: socket");
-         continue;
-      }
-
-      if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
-               sizeof(int)) == -1) {
-         perror("setsockopt");
-         exit(1);
-      }
-
-      if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-         close(sockfd);
-         perror("server: bind");
-         continue;
-      }
-
-      break;
-   }
-
-   freeaddrinfo(servinfo); // all done with this structure
-
-   if (p == NULL)  {
-      fprintf(stderr, "server: failed to bind\n");
-      exit(1);
-   }
-
-   if (listen(sockfd, BACKLOG) == -1) {
-      perror("listen");
-      exit(1);
-   }
-
-   sa.sa_handler = sigchld_handler; // reap all dead processes
-   sigemptyset(&sa.sa_mask);
-   sa.sa_flags = SA_RESTART;
-   if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-      perror("sigaction");
-      exit(1);
-   }
-
-   printf("server: waiting for connections...\n");
-
-   while(1) {  // main accept() loop
-      sin_size = sizeof their_addr;
-      new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-      if (new_fd == -1) {
-         perror("accept");
-         continue;
-      }
-
-      inet_ntop(their_addr.ss_family,
-         get_in_addr((struct sockaddr *)&their_addr),
-         s, sizeof s);
-      printf("server: got connection from %s\n", s);
-
-      if (send(new_fd, "Hello, world!", 13, 0) == -1)
-         perror("send");
-   }
+   int PORT = atoi(argv[2]);
+   int opt = TRUE;  
+   int master_socket, addrlen, new_socket, client_socket[64], 
+         max_clients = 64, activity, sd;  
+   int max_sd;  
+   long valread;
+   struct sockaddr_in address;  
+   std::map<int, unsigned long> worker_map;
+   std::queue<unsigned long> failed;
+   char buffer[1025];  //data buffer of 1K 
+   
+   //set of socket descriptors 
+   fd_set readfds;  
 
    auto curlSetup = CurlGlobalSetup();
-
    auto listUrl = std::string(argv[1]);
 
    // Download the file list
@@ -145,34 +73,180 @@ int main(int argc, char* argv[]) {
    curl.setUrl(listUrl);
    auto fileList = curl.performToStringStream();
 
-   size_t result = 0;
+   unsigned long total = 0;
    // Iterate over all files
-   for (std::string url; std::getline(fileList, url, '\n');) {
-      curl.setUrl(url);
-      // Download them
-      auto csvData = curl.performToStringStream();
-      for (std::string row; std::getline(csvData, row, '\n');) {
-         auto rowStream = std::stringstream(std::move(row));
 
-         // Check the URL in the second column
-         unsigned columnIndex = 0;
-         for (std::string column; std::getline(rowStream, column, '\t'); ++columnIndex) {
-            // column 0 is id, 1 is URL
-            if (columnIndex == 1) {
-               // Check if URL is "google.ru"
-               auto pos = column.find("://"sv);
-               if (pos != std::string::npos) {
-                  auto afterProtocol = std::string_view(column).substr(pos + 3);
-                  if (afterProtocol.starts_with("google.ru/"))
-                     ++result;
-               }
-               break;
-            }
-         }
-      }
+   std::vector<std::string> urls;
+   std::string url;
+   while (std::getline(fileList, url, '\n')) {
+      // Line contains string of length > 0 then save it in vector
+      if(url.size() > 0)
+         urls.push_back(url);
    }
+   unsigned long url_count = urls.size();
 
-   std::cout << result << std::endl;
+   //initialise all client_socket[] to 0 so not checked 
+   for (int i = 0; i < max_clients; i++) {  
+      client_socket[i] = 0;  
+   }  
+
+   //create a master socket 
+   if((master_socket = socket(AF_INET , SOCK_STREAM , 0)) == 0) {  
+      perror("coordinator: socket failed");  
+      exit(EXIT_FAILURE);  
+   } 
+
+   //set coordinator socket to allow multiple connections , 
+   //this is just a good habit, it will work without this 
+   if(setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) < 0) {  
+      perror("coordinator: setsockopt");  
+      exit(EXIT_FAILURE);  
+   }  
+
+   //type of socket created 
+   address.sin_family = AF_INET;  
+   address.sin_addr.s_addr = INADDR_ANY;  
+   address.sin_port = htons(PORT); 
+
+   //bind the socket to localhost port 8888 
+   if (bind(master_socket, (struct sockaddr *)&address, sizeof(address)) < 0) {  
+      perror("coordinator: bind failed");  
+      exit(EXIT_FAILURE);  
+   }  
+   printf("coordinator: listening on port %d \n", PORT); 
+
+   if (listen(master_socket, BACKLOG) < 0) {  
+      perror("coordinator: listen");  
+      exit(EXIT_FAILURE);  
+   }  
+
+   //accept the incoming connection 
+   addrlen = sizeof(address);  
+   puts("coordinator: waiting for connections...");  
+
+   unsigned url_idx = 0;
+   unsigned received = 0;
+   while (received < url_count) {  // main accept() loop
+      //clear the socket set 
+      FD_ZERO(&readfds);  
+
+      //add master socket to set 
+      FD_SET(master_socket, &readfds);  
+      max_sd = master_socket;  
+            
+      //add child sockets to set 
+      for (int i = 0; i < max_clients; i++) {  
+         //socket descriptor 
+         sd = client_socket[i];  
+               
+         //if valid socket descriptor then add to read list 
+         if (sd > 0)  
+            FD_SET(sd , &readfds);  
+               
+         //highest file descriptor number, need it for the select function 
+         if (sd > max_sd)  
+            max_sd = sd;  
+      }  
+
+      //wait for an activity on one of the sockets , timeout is NULL , 
+      //so wait indefinitely 
+      activity = select(max_sd + 1 , &readfds , NULL , NULL , NULL); 
+
+      if ((activity < 0) && (errno != EINTR)) {  
+         printf("coordinator: select error");  
+      }  
+
+      if (FD_ISSET(master_socket, &readfds)) {  
+         if ((new_socket = accept(master_socket, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {  
+            perror("coordinator: accept");  
+            exit(EXIT_FAILURE);  
+         }  
+
+         //inform user of socket number - used in send and receive commands 
+         printf("New connection, socket fd: %d, IP: %s, port: %d\n", 
+            new_socket, inet_ntoa(address.sin_addr), ntohs(address.sin_port)); 
+
+         //send new connection next url 
+         const char* next_url = urls[url_idx].c_str();
+         if(send(new_socket, next_url, strlen(next_url), 0) == 0)  {  
+            perror("coordinator: send");  
+         }  
+         std::cout << "coordinator: sent url: " << next_url << std::endl;
+         worker_map[new_socket] = url_idx;
+         url_idx++;
+
+         //add new socket to array of sockets 
+         for (int i = 0; i < max_clients; i++) {  
+            //if position is empty 
+            if(client_socket[i] == 0) {  
+               client_socket[i] = new_socket;  
+               printf("Adding to list of sockets as %d\n", i);                 
+               break;  
+            }  
+         }  
+      }
+
+      //else its some IO operation on some other socket
+      for (int i = 0; i < max_clients; i++) {  
+         sd = client_socket[i];  
+               
+         if (FD_ISSET(sd, &readfds)) {  
+            //Check if it was for closing , and also read the 
+            //incoming message 
+            if ((valread = read(sd, buffer, MAXDATASIZE)) == 0) {  
+               //Somebody disconnected , get his details and print 
+               getpeername(sd, (struct sockaddr*)&address, (socklen_t*)&addrlen);  
+               printf("Host disconnected, IP: %s, port: %d \n", 
+                  inet_ntoa(address.sin_addr) , ntohs(address.sin_port));  
+               
+               if (worker_map.find(sd) != worker_map.end()) {
+                  failed.push(worker_map[sd]);
+               }
+                     
+               //Close the socket and mark as 0 in list for reuse 
+               close(sd);  
+               client_socket[i] = 0;  
+            }  
+                  
+            //Process the result that came back
+            else {  
+               //set the string terminating NULL byte on the end 
+               //of the data read 
+               buffer[valread] = '\0';  
+               std::string s(buffer);
+               unsigned long idx = stoul(s.substr(0, s.find(',')));
+               unsigned long result = stoul(s.substr(s.find(',') + 1));
+               std::cout << "coordinator: received result: " << result << " for url: " << urls[idx] << std::endl;
+               worker_map.erase(sd);
+               total += result;
+               received++;
+
+               if (url_idx < url_count) {
+                  //send new connection next url 
+                  const char* next_url = urls[url_idx].c_str();
+                  if(send(sd, next_url, strlen(next_url), 0) == 0)  {  
+                     perror("coordinator: send");  
+                  }  
+                  std::cout << "coordinator: sent url: " << next_url << std::endl;
+                  worker_map[new_socket] = url_idx;
+                  url_idx++;
+               }
+               else if (failed.size() > 0) {
+                  const char* next_url = urls[failed.front()].c_str();
+                  if(send(sd, next_url, strlen(next_url), 0) == 0)  {  
+                     perror("coordinator: send");  
+                  }
+                  std::cout << "coordinator: sent url: " << next_url << std::endl;
+                  worker_map[new_socket] = failed.front();
+                  failed.pop();
+               }
+            }  
+         }  
+      }  
+   }
+   //close socket
+   close(master_socket);
+   std::cout << "total: " << total << std::endl;
 
    return 0;
 }
--- worker.cpp
+++ worker.cpp
@@ -1,4 +1,6 @@
+#include "CurlEasyPtr.h"
 #include <iostream>
+#include <sstream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -10,7 +12,10 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
-#define MAXDATASIZE 100 // max number of bytes we can get at once 
+using namespace std::literals;
+
+#define MAXDATASIZE 1024 // max number of bytes we can get at once 
+#define MAXCONNATTEMPTS 5
 
 void *get_in_addr(struct sockaddr *sa) {
     if (sa->sa_family == AF_INET) {
@@ -20,83 +25,124 @@ void *get_in_addr(struct sockaddr *sa) {
     return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
+unsigned long get_url_index(char* url) {
+   //url format is http://example.org/blah/blah/blah.urlindex.csv
+   //so we need to find the last two . and get the number in between
+
+   std::string url_str(url);
+   unsigned long url_index = 0;
+   unsigned int last_dot = 0;
+   unsigned int second_last_dot = 0;
+
+   for (unsigned int i = 0; i < url_str.length(); i++) {
+      if (url_str[i] == '.') {
+         second_last_dot = last_dot;
+         last_dot = i;
+      }
+   }
+
+   std::string url_index_str = url_str.substr(second_last_dot + 1, last_dot);
+   url_index = std::stoul(url_index_str);
+
+   return url_index;
+}
+
 /// Worker process that receives a list of URLs and reports the result
 /// Example:
 ///    ./worker localhost 4242
 /// The worker then contacts the leader process on "localhost" port "4242" for work
 int main(int argc, char* argv[]) {
+   std::cout << "worker: start" << std::endl;
    if (argc != 3) {
       std::cerr << "Usage: " << argv[0] << " <host> <port>" << std::endl;
       return 1;
    }
 
-   // TODO:
-   //    1. connect to coordinator specified by host and port
-   //       getaddrinfo(), connect(), see: https://beej.us/guide/bgnet/html/#system-calls-or-bust
-   //    2. receive work from coordinator
-   //       recv(), matching the coordinator's send() work
-   //    3. process work
-   //       see coordinator.cpp
-   //    4. report result
-   //       send(), matching the coordinator's recv()
-   //    5. repeat
+   int PORT = atoi(argv[2]);
+   int sock = 0, client_fd = -1;
+   long valread;
+   struct sockaddr_in serv_addr;
+   char buffer[1024] = { 0 };
+   auto curlSetup = CurlGlobalSetup();
+   auto curl = CurlEasyPtr::easyInit();
 
-   const char* PORT = argv[2];
-   int sockfd;
-   long numbytes;  
-   char buf[MAXDATASIZE];
-   struct addrinfo hints, *servinfo, *p;
-   int rv;
-   char s[INET6_ADDRSTRLEN];
-
-   memset(&hints, 0, sizeof hints);
-   hints.ai_family = AF_UNSPEC;
-   hints.ai_socktype = SOCK_STREAM;
-
-   if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) {
-      fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-      return 1;
-   }
-
-   // loop through all the results and connect to the first we can
-   for(p = servinfo; p != NULL; p = p->ai_next) {
-      if ((sockfd = socket(p->ai_family, p->ai_socktype,
-               p->ai_protocol)) == -1) {
-         perror("client: socket");
-         continue;
+   int retries = 0;
+   while (client_fd < 0) {
+      if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+         printf("\n Socket creation error \n");
+         return -1;
       }
 
-      if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-         close(sockfd);
-         perror("client: connect");
-         continue;
+      serv_addr.sin_family = AF_INET;
+      serv_addr.sin_port = htons(PORT);
+
+      // Convert IPv4 and IPv6 addresses from text to binary
+      // form
+      if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+         printf("\nInvalid address/ Address not supported. \n");
+         return -1;
       }
 
-      break;
+      std::cout << "worker: waiting for the server" << std::endl; 
+      //sleep(2);
+   
+      if ((client_fd = connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)))
+         < 0) {
+         printf("\nworker: connection failed. retrying...\n");
+         retries++;
+         close(sock);
+         sleep(1);
+      }
+
+      if (retries > MAXCONNATTEMPTS) {
+         printf("\nworker: connection failed. max retries reached. exiting...\n");
+         return -1;
+      }
    }
 
-   if (p == NULL) {
-      fprintf(stderr, "client: failed to connect\n");
-      return 2;
+   while(1) {
+      if ((valread = recv(sock, buffer, MAXDATASIZE-1, 0)) <= 0) {
+         exit(1);
+      }
+
+      buffer[valread] = '\0';
+
+      std::cout << "worker: received: " << buffer << std::endl;
+      
+      size_t result = 0;
+      curl.setUrl(buffer);
+      // Download them
+      auto csvData = curl.performToStringStream();
+      for (std::string row; std::getline(csvData, row, '\n');) {
+         auto rowStream = std::stringstream(std::move(row));
+
+         // Check the URL in the second column
+         unsigned columnIndex = 0;
+         for (std::string column; std::getline(rowStream, column, '\t'); ++columnIndex) {
+            // column 0 is id, 1 is URL
+            if (columnIndex == 1) {
+               // Check if URL is "google.ru"
+               auto pos = column.find("://"sv);
+               if (pos != std::string::npos) {
+                  auto afterProtocol = std::string_view(column).substr(pos + 3);
+                  if (afterProtocol.starts_with("google.ru/"))
+                     ++result;
+               }
+               break;
+            }
+         }
+      }
+
+      unsigned long url_index = get_url_index(buffer);
+      std::string result_with_index = std::to_string(url_index) + "," + std::to_string(result);
+
+      //send result back to coordinator
+      if (send(sock, result_with_index.c_str(), result_with_index.length(), 0) == -1)
+         perror("send");
+
+      std::cout << "worker: result: " << result << std::endl;
    }
 
-   inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
-         s, sizeof s);
-   printf("client: connecting to %s\n", s);
-
-   freeaddrinfo(servinfo); // all done with this structure
-
-   if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
-      perror("recv");
-      exit(1);
-   }
-
-   buf[numbytes] = '\0';
-
-   printf("client: received '%s'\n",buf);
-
-   close(sockfd);
-
-
+   close(sock);
    return 0;
 }
